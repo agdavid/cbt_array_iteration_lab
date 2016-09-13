ce1d7901f14c34a21616e4f89a459e44b55622d5
agdavid <antonio.g.david@gmail.com>
2016-09-13 08:08:07 -0400

    create JS file with array and function shell

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document.ready(
+  var myArray = [
+    "apples",
+    "oranges",
+    "bananas",
+    "kiwi"
+  ]
+
+  function showArray() {
+    for(var i = 0; i < myArray.length; i++) {
+      
+    }
+  };
+
+  ));

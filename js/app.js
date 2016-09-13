fe9683dc654074854578bfffe35e0e9f9149e5a1
agdavid <antonio.g.david@gmail.com>
2016-09-13 08:33:02 -0400

    Done. Working array iteration example.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,24 @@
-$(document.ready(
-  var myArray = [
-    "apples",
-    "oranges",
-    "bananas",
-    "kiwi"
-  ]
+console.log("ready!");
 
-  var resultsDiv = document.getElementById('resultsDiv')
+var myArray = [
+  "apples",
+  "oranges",
+  "bananas",
+  "kiwi"
+];
 
-  function showArray() {
-    for(var i = 0; i < myArray.length; i++) {
-      var childDiv = document.createElement('div')
-      childDiv.innerHTML(
-        "Item number " + (i+1).toString() + " at index " + i.toString() + " is " + myArray[i]
-        );
-      resultsDiv.append(childDiv);
-    }
-  };
+function showArray() {
+  
+  var resultsDiv = document.getElementById('resultsDiv');
 
-  ));
+  for(var i = 0; i < myArray.length; i++) {
+    var childDiv = document.createElement('div');
+    var string = "Item number " + (i+1).toString() + " at index " + i.toString() + " is " + myArray[i];
+    childDiv.innerHTML = string;
+    resultsDiv.appendChild(childDiv);
+  }
+};
+
+
+
+

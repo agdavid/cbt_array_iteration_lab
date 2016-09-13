b0b8e778dc3e797d32369328b4c046960c363acd
agdavid <antonio.g.david@gmail.com>
2016-09-13 08:13:38 -0400

    add showArray() function to JS

--- js/app.js
+++ js/app.js
@@ -6,9 +6,15 @@ $(document.ready(
     "kiwi"
   ]
 
+  var resultsDiv = document.getElementById('resultsDiv')
+
   function showArray() {
     for(var i = 0; i < myArray.length; i++) {
-      
+      var childDiv = document.createElement('div')
+      childDiv.innerHTML(
+        "Item number " + (i+1).toString() + " at index " + i.toString() + " is " + myArray[i]
+        );
+      resultsDiv.append(childDiv);
     }
   };
 
